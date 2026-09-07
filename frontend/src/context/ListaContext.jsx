9a7fd50e6ef9b075fdc3d329ffae4ff537fae91c
Bno9 <breno_live2005@hotmail.com>
2026-09-07 00:32:39 -0300

    rotas locais alteradas

--- frontend/src/context/ListaContext.jsx
+++ frontend/src/context/ListaContext.jsx
@@ -8,7 +8,7 @@ function ListaProvider({ children }) {
   const [lista, setLista] = useState([])
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/lista')
+    fetch(`${import.meta.env.VITE_API_URL}/lista`)
       .then((response) => response.json())
       .then((data) => setLista(data.produtos))
       .catch((error) => console.error('Erro ao buscar a lista:', error))
