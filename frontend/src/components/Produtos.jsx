343e04931c0bf19113b6b86d714d46c6250303b2
Bno9 <breno_live2005@hotmail.com>
2026-09-06 21:22:41 -0300

    Ajuste no endpoint de categorias

--- frontend/src/components/Produtos.jsx
+++ frontend/src/components/Produtos.jsx
@@ -1,12 +1,60 @@
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import ListaContext from "../context/ListaContext"
 
 function Produtos() {
+  const [categorias, setCategorias] = useState([])
   const { lista, setLista } = useContext(ListaContext)
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/categorias")
+        .then(response => response.json())
+        .then(data => {
+            setCategorias(data)
+        })
+  }, [])
+
+  function AdicionarProdutoLista(produto){
+    fetch("http://127.0.0.1:5000/lista", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: {"id_produto": produto.id,
+            "quantidade": produto.quantidade ? produto.quantidade : 1
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setLista((prevLista) => [...prevLista, ...data])
+    })
+  }
 
   return (
     <div>
-      <h2 className="flex justify-center font-bold uppercase">Lista de Produtos</h2>
+      <h2 className="flex justify-center font-bold uppercase">Produtos cadastrados</h2>
+
+      {categorias.map((categoria) => (
+        <div key={categoria.id} className="mb-4">
+
+          <h3 className="font-bold">{categoria.nome}</h3>
+
+          <ul className="list-disc pl-5">
+            
+            {categoria.produtos.map((produto) => (
+              <li key={produto.id} className="flex justify-between items-center">
+
+                <span>{produto.nome}</span>
+
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                  onClick={() => AdicionarProdutoLista(produto)}
+                >Adicionar à lista</button>
+
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   )
 }
