732d02575fa9bfca9eb0d5494f2115847a3f87bc
Bno9 <breno_live2005@hotmail.com>
2026-09-11 00:49:32 -0300

    tests/fix endpoints lista, adição fixture pro app, e teste da atualização de produto

--- Backend/tests/test_endpoint_list.py
+++ Backend/tests/test_endpoint_list.py
@@ -1,30 +1,6 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from tests.fixtures.testClient import client
+from tests.fixtures.produto_teste import criar_produto_teste
 import pytest
-
-client = TestClient(app)
-
-@pytest.fixture
-def criar_produto_teste():
-    # Cria uma categoria
-    client.post(
-        "/categorias",
-        json={"name": "Eletrônicos"}
-    )
-    categoria_name = response.json()["nome"]
-
-    # Cria um produto nessa categoria
-    response = client.post(
-        "/produtos",
-        json={
-            "name": "Smartphone",
-            "categoria": categoria_name
-        }
-    )
-
-    data = response.json()
-
-    return data["id"]
 
 def test_salvar_produto_lista(override_get_db, criar_produto_teste):
     produto_id = criar_produto_teste
@@ -32,7 +8,7 @@ def test_salvar_produto_lista(override_get_db, criar_produto_teste):
     response = client.post(
         "/lista",
         json={
-            "produto_id": produto_id,
+            "id_produto": produto_id,
             "quantidade": 1
         }
     )
@@ -40,7 +16,7 @@ def test_salvar_produto_lista(override_get_db, criar_produto_teste):
     data = response.json()
 
     assert response.status_code == 201
-    assert data["message"] == "Produto salvo na lista"
+    assert  "id" in data
 
 def test_retornar_lista(override_get_db):
     response = client.get(
@@ -50,26 +26,43 @@ def test_retornar_lista(override_get_db):
     data = response.json()
 
     assert response.status_code == 200
-    assert type(data["message"]) == list
+    assert type(data["produtos"]) == list
     assert data is not None
 
 def test_atualizar_quantidade_lista(override_get_db, criar_produto_teste):
     id_produto = criar_produto_teste
 
-    response = client.put(
-        f"/lista/{id_produto}",
+    client.post(
+        f"/lista/",
         json={
+            "id_produto": id_produto,
+            "quantidade": 2
+        }
+        )
+    
+    response = client.post(
+        f"/lista/",
+        json={
+            "id_produto": id_produto,
             "quantidade": 2
         }
         )
     
     data = response.json()
 
-    assert response.status_code == 200
-    assert data["message"] == "Quantidade do produto atualizada"
+    assert response.status_code == 201
+    assert data["message"] == "Quantidade do produto atualizada na lista"
 
 def test_excluir_produto_lista(override_get_db, criar_produto_teste):
     id_produto = criar_produto_teste
+
+    client.post(
+        f"/lista/",
+        json={
+            "id_produto": id_produto,
+            "quantidade": 2
+        }
+        )
 
     response = client.delete(
         f"/lista/{id_produto}"
--- Backend/tests/test_endpoints_produtos.py
+++ Backend/tests/test_endpoints_produtos.py
@@ -1,9 +1,5 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-# Todos produtos precisam ter uma categoria, então primeiro criamos a categoria e depois o produto.
+from tests.fixtures.testClient import client
+from tests.fixtures.produto_teste import criar_produto_teste
 
 def test_criar_produto(override_get_db):
     response = client.post(
@@ -29,23 +25,8 @@ def test_criar_produto(override_get_db):
     assert data["categoria"] == "Ferramentas"
     assert "id" in data
 
-def test_deletar_produto(override_get_db):
-    response = client.post(
-        "/categorias",
-        json={"name": "Ferramentas"}
-    )
-
-    # Cria um produto
-    response = client.post(
-        "/produtos",
-        json={
-            "name": "Chave de Fenda",
-            "categoria": "Ferramentas"
-        }
-    )
-    assert response.status_code == 201
-
-    produto_id = response.json()["id"]
+def test_deletar_produto(override_get_db, criar_produto_teste):
+    produto_id = criar_produto_teste
 
     # Deleta o produto
     response = client.delete(f"/produtos/{produto_id}")
@@ -60,3 +41,21 @@ def test_deletar_produto_inexistente(override_get_db):
 
     data = response.json()
     assert data["detail"] == "Produto não encontrado"
+
+def test_atualizar_produto(override_get_db, criar_produto_teste):
+    produto_id = criar_produto_teste
+
+    client.post(
+        "/categorias",
+        json={"name": "Teste"}
+    )
+
+    response = client.put(f"/produtos/{produto_id}",
+                          json={
+                              "name": "Teste",
+                              "categoria_id": 2
+                          })
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["message"] == "Produto atualizado"
